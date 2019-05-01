ca1ea5b7de3af5bdccb8802d711e1894c6355391
subby <subby8pie@gmail.com>
2019-05-01 09:02:33 -0700

    Added travis files

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,21 @@
+package edu.ucsd.cs110.temperature;
+
+public class Celsius extends Temperature {
+    public Celsius(float t) {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return this;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return new Fahrenheit((this.getValue() * (9f / 5)) + 32);
+    }
+
+    public String toString() {
+        return this.getValue() + " C";
+    }
+}
